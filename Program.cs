e2e9f1625094123e011552cd0f4a4e95d776c003
Alexandra <alexadudki11@gmail.com>
2023-06-06 14:05:19 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -27,9 +27,26 @@ void PrintArray(int[,] matrix){
     }
     Console.WriteLine();
 }
-int[,] array = new int[7, 4];
+int[,] array = new int[4, 4];
 MakeArray(array);
 PrintArray(array);
 SortLine(array);
 PrintArray(array);
 //Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+void MinSumLine(int[,] matrix){
+    int minn=1000;
+    for (int i = 0; i < matrix.GetLength(0); i++){
+        int summ = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++){
+            summ+=matrix[i,j];
+        }
+        if(minn>summ) summ=minn;
+    }
+    Console.Write($"Минимальная сумма среди строк: {minn}");
+    Console.WriteLine();
+}
+int[,] array1 = new int[7, 3];
+MakeArray(array1);
+PrintArray(array1);
+MinSumLine(array1);
+
